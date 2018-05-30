57ac5b5c758ce6fb41875c3e8b105c3aa44aa862
adamsb0713 <briadams2016@gmail.com>
2018-05-30 23:52:22 +1200

    commit

--- Scripts/firstPlot.r
+++ Scripts/firstPlot.r
@@ -3,7 +3,7 @@
   packageData = read.csv("Data/CRANpackages.csv");
   
   plotData = ggplot(packageData) +
-    geom_point( aes(x=Date, y=Packages) ) +
+    geom_point( aes(x=(Date), y=Packages) ) +
     ggtitle("Packages in CRAN (2001-2014)") +
     scale_y_continuous(breaks = seq(0, 6000, by=500)) +
     theme(axis.text.x=element_text(angle=90, hjust=1));
--- Scripts/lesson2.r
+++ Scripts/lesson2.r
@@ -4,22 +4,26 @@
                          stringsAsFactors = FALSE);
   
   plotData = ggplot(weatherData) +
-    geom_point(aes(x=avgTemp, y=relHum),
-               color="darkgreen", size=2.5, shape=17,
+    geom_point(aes(x=windSpeed, y=abs(tempDept)),
+               color="purple",size=3.5, shape=5,
                alpha = 0.4 ) +
     theme_bw() +
     labs(title = "Humidity vs. Temperature",
          subtitle = "Lansing, Michigan: 2016",
-         x = "Temperature (F)",
-         y = "Humidity (%)") +
-    theme(axis.title.x=element_text(size=14, color="orangered2"),
-          axis.title.y=element_text(size=14, color="orangered4"),
-          plot.title=element_text(size=18, face="bold",
-                                  color ="darkblue"),
+         x = "Wind Speed (m/s)",
+         y = "Temperature Departure (F)") +
+    theme(axis.title.x=element_text(size=14, color="lightblue"),
+          axis.title.y=element_text(size=14, color="lightblue"),
+          plot.title=element_text(size=19, face="bold",
+                                  color ="navy"),
           plot.subtitle=element_text(size=10, face="bold.italic",
-                                     color ="brown", family="serif")) +
-    geom_smooth(mapping=aes(x=avgTemp, y=relHum), method="lm",
-                color="red", size=0.8, linetype=4, fill="lightblue");
+                                     color ="black", family="serif")) +
+    geom_smooth(mapping=aes(x=windSpeed, y=tempDept), method="lm",
+                color="darkgreen", size=0.8, linetype=4, fill="lightgrey") +
+    theme(axis.text.x=element_text(angle=45, hjust=1)) +
+    theme(axis.text.y=element_text(angle=90, hjust=1));
   
   plot(plotData);
+  
+  cat("This graph shows Wind Speed v. Temperature Departure");
 }
